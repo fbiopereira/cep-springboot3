acd3e142f2b5321c2197d5e5b905281f6a91b4de
Fábio Pereira <fabio.pereira@outlook.com>
2023-08-22 19:33:36 -0300

    criando projeto spring

---
+++ src/main/java/com/fbiopereira/cep/cepspringboot3/CepSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.cep.cepspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CepSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fbiopereira/cep/cepspringboot3/CepSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.cep.cepspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
